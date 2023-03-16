698ad6e1072a9d9b974f6b39d4d68a206b0a83b9
admin <dimon.sudak2015@gmail.com>
2023-03-16 19:01:23 +0300

    срочный комит

--- src/main/java/ru/spmi/backend/repositories/TestRepository.java
+++ src/main/java/ru/spmi/backend/repositories/TestRepository.java
@@ -1,6 +1,8 @@
 package ru.spmi.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
 import ru.spmi.backend.entities.TestEntity;
 
@@ -9,7 +11,8 @@ import java.util.ArrayList;
 @Repository
 public interface TestRepository extends JpaRepository<TestEntity, Long> {
 
-    ArrayList<EmployeeResponse> listEployeers(String filters, int page_rows, int page_num);
 
+    @Procedure("vf_test_complete")
+    ArrayList<TestEntity> vf
 
 }
